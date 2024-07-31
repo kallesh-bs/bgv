abe9dea4fe5eb9176650d5642e1d3b53f5d3e883
Kallesh B S <kallesh.bs@deeporion.com>
2024-07-31 17:20:56 +0530

    added test cases for side-drawer bgv

---
+++ src/component/admin/Verification/__test__/ExtraActions.test.tsx
@@ -0,0 +1,218 @@
+import { render, screen } from '@testing-library/react';
+import ExtraActions from '../ExtraActions';
+import { Provider } from "react-redux";
+import store from "../../../../redux/store";
+import "@testing-library/jest-dom";
+import i18n from 'i18next';
+import { Trans } from 'react-i18next';
+import { I18nextProvider } from "react-i18next";
+import { VerificationDataKey, VerificationSidePopUpNavTabName } from '../types';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore([]);
+
+i18n.init({
+  lng: 'en', // set the default language
+  resources: {
+    en: {
+      translation: {
+        // add your translations here
+      }
+    }
+  }
+});
+
+describe("Forgot Component", () => {
+    let store: any;
+
+
+    beforeEach(() => {
+        store = mockStore({
+          bgvReducer: {
+            employeeData: [],
+            isLoading: true,
+            employeeDataById: {"background_verification": {
+                "id": 37,
+                "user_id": 3,
+                "identity_check_documents": null,
+                "identity_check_documents_status": "in_progress",
+                "markshseet_10th": null,
+                "markshseet_10th_status": "in_progress",
+                "markshseet_12th": null,
+                "markshseet_12th_status": "in_progress",
+                "graduation_degrees": null,
+                "graduation_degrees_status": "in_progress",
+                "post_graduation_degrees": null,
+                "post_graduation_degrees_status": "in_progress",
+                "other_certifications": null,
+                "other_certifications_status": "in_progress",
+                "address_check_documents": null,
+                "address_check_documents_status": "in_progress",
+                "relieving_letters": null,
+                "relieving_letters_status": "in_progress",
+                "experience_letters": null,
+                "experience_letters_status": "in_progress",
+                "bank_statements": null,
+                "bank_statements_status": "in_progress",
+                "hold": true
+            }},
+            profileCompletionById: {},
+            sidePopUpDocNavTab: 1
+          },
+        });
+      });
+
+  it('renders learn react link', () => {
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <ExtraActions nodata={false} doc_status_column={''} doc_column={''} />
+              </Provider>
+          </I18nextProvider>
+      );
+  });
+
+  it('renders learn react link', () => {
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <ExtraActions nodata={true} doc_status_column={VerificationDataKey.IDENTITY_CHECK_DOCUMENTS_STATUS} doc_column={VerificationDataKey.IDENTITY_CHECK_DOCUMENTS} />
+              </Provider>
+          </I18nextProvider>
+      );
+  });
+
+  it('renders learn react link', () => {
+    store = mockStore({
+        bgvReducer: {
+          employeeData: [],
+          isLoading: true,
+          employeeDataById: {"background_verification": {
+              "id": 37,
+              "user_id": 3,
+              "identity_check_documents": null,
+              "identity_check_documents_status": "in_progress",
+              "markshseet_10th": null,
+              "markshseet_10th_status": "in_progress",
+              "markshseet_12th": null,
+              "markshseet_12th_status": "in_progress",
+              "graduation_degrees": null,
+              "graduation_degrees_status": "in_progress",
+              "post_graduation_degrees": null,
+              "post_graduation_degrees_status": "in_progress",
+              "other_certifications": null,
+              "other_certifications_status": "in_progress",
+              "address_check_documents": null,
+              "address_check_documents_status": "in_progress",
+              "relieving_letters": null,
+              "relieving_letters_status": "in_progress",
+              "experience_letters": null,
+              "experience_letters_status": "in_progress",
+              "bank_statements": null,
+              "bank_statements_status": "in_progress",
+              "hold": true
+          }},
+          profileCompletionById: {},
+          sidePopUpDocNavTab: 2
+        },
+      });
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <ExtraActions nodata={true} doc_status_column={VerificationDataKey.ADDRESS_CHECK_DOUCMENTS_STATUS} doc_column={VerificationDataKey.ADDRESS_CHECK_DOUCMENTS} />
+              </Provider>
+          </I18nextProvider>
+      );
+  });
+
+  it('renders learn react link', () => {
+
+    store = mockStore({
+        bgvReducer: {
+          employeeData: [],
+          isLoading: true,
+          employeeDataById: {"background_verification": {
+              "id": 37,
+              "user_id": 3,
+              "identity_check_documents": null,
+              "identity_check_documents_status": "in_progress",
+              "markshseet_10th": null,
+              "markshseet_10th_status": "in_progress",
+              "markshseet_12th": null,
+              "markshseet_12th_status": "in_progress",
+              "graduation_degrees": null,
+              "graduation_degrees_status": "in_progress",
+              "post_graduation_degrees": null,
+              "post_graduation_degrees_status": "in_progress",
+              "other_certifications": null,
+              "other_certifications_status": "in_progress",
+              "address_check_documents": null,
+              "address_check_documents_status": "in_progress",
+              "relieving_letters": null,
+              "relieving_letters_status": "in_progress",
+              "experience_letters": null,
+              "experience_letters_status": "in_progress",
+              "bank_statements": null,
+              "bank_statements_status": "in_progress",
+              "hold": true
+          }},
+          profileCompletionById: {},
+          sidePopUpDocNavTab: 3
+        },
+      });
+
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <ExtraActions nodata={true} doc_status_column={VerificationDataKey.ADDRESS_CHECK_DOUCMENTS_STATUS} doc_column={VerificationDataKey.ADDRESS_CHECK_DOUCMENTS} />
+              </Provider>
+          </I18nextProvider>
+      );
+  });
+
+  it('renders learn react link', () => {
+
+    store = mockStore({
+        bgvReducer: {
+          employeeData: [],
+          isLoading: true,
+          employeeDataById: {"background_verification": {
+              "id": 37,
+              "user_id": 3,
+              "identity_check_documents": null,
+              "identity_check_documents_status": "in_progress",
+              "markshseet_10th": null,
+              "markshseet_10th_status": "in_progress",
+              "markshseet_12th": null,
+              "markshseet_12th_status": "in_progress",
+              "graduation_degrees": null,
+              "graduation_degrees_status": "in_progress",
+              "post_graduation_degrees": null,
+              "post_graduation_degrees_status": "in_progress",
+              "other_certifications": null,
+              "other_certifications_status": "in_progress",
+              "address_check_documents": null,
+              "address_check_documents_status": "in_progress",
+              "relieving_letters": null,
+              "relieving_letters_status": "in_progress",
+              "experience_letters": null,
+              "experience_letters_status": "in_progress",
+              "bank_statements": null,
+              "bank_statements_status": "in_progress",
+              "hold": true
+          }},
+          profileCompletionById: {},
+          sidePopUpDocNavTab: 4
+        },
+      });
+
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <ExtraActions nodata={true} doc_status_column={VerificationDataKey.ADDRESS_CHECK_DOUCMENTS_STATUS} doc_column={VerificationDataKey.ADDRESS_CHECK_DOUCMENTS} />
+              </Provider>
+          </I18nextProvider>
+      );
+  });
+
+})
