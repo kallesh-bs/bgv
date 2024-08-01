a16329ecce533c2e6b5c2d16b6c0f4544225e908
Kallesh B S <kallesh.bs@deeporion.com>
2024-08-01 10:33:18 +0530

    completed DropDownDocument test cases

--- src/component/admin/Verification/__test__/DocumentDropDown.test.tsx
+++ src/component/admin/Verification/__test__/DocumentDropDown.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import DocumentDropDown from '../DocumentDropDown';
 import { Provider } from "react-redux";
 import store from "../../../../redux/store";
@@ -26,43 +26,42 @@ describe("Forgot Component", () => {
     let store: any;
 
 
-    beforeEach(() => {
-        store = mockStore({
-          bgvReducer: {
-            employeeData: [],
-            isLoading: true,
-            employeeDataById: {"background_verification": {
-                "id": 37,
-                "user_id": 3,
-                "identity_check_documents": null,
-                "identity_check_documents_status": "in_progress",
-                "markshseet_10th": null,
-                "markshseet_10th_status": "in_progress",
-                "markshseet_12th": null,
-                "markshseet_12th_status": "in_progress",
-                "graduation_degrees": null,
-                "graduation_degrees_status": "in_progress",
-                "post_graduation_degrees": null,
-                "post_graduation_degrees_status": "in_progress",
-                "other_certifications": null,
-                "other_certifications_status": "in_progress",
-                "address_check_documents": null,
-                "address_check_documents_status": "in_progress",
-                "relieving_letters": null,
-                "relieving_letters_status": "in_progress",
-                "experience_letters": null,
-                "experience_letters_status": "in_progress",
-                "bank_statements": null,
-                "bank_statements_status": "in_progress",
-                "hold": true
-            }},
-            profileCompletionById: {},
-            sidePopUpDocNavTab: 1
-          },
-        });
-      });
-
   it('renders learn react link', () => {
+
+    store = mockStore({
+      bgvReducer: {
+        employeeData: [],
+        isLoading: true,
+        employeeDataById: {"background_verification": {
+            "id": 37,
+            "user_id": 3,
+            "identity_check_documents": null,
+            "identity_check_documents_status": "in_progress",
+            "markshseet_10th": null,
+            "markshseet_10th_status": "in_progress",
+            "markshseet_12th": null,
+            "markshseet_12th_status": "in_progress",
+            "graduation_degrees": null,
+            "graduation_degrees_status": "in_progress",
+            "post_graduation_degrees": null,
+            "post_graduation_degrees_status": "in_progress",
+            "other_certifications": null,
+            "other_certifications_status": "in_progress",
+            "address_check_documents": null,
+            "address_check_documents_status": "in_progress",
+            "relieving_letters": null,
+            "relieving_letters_status": "in_progress",
+            "experience_letters": null,
+            "experience_letters_status": "in_progress",
+            "bank_statements": null,
+            "bank_statements_status": "in_progress",
+            "hold": true
+        }},
+        profileCompletionById: {},
+        sidePopUpDocNavTab: 1
+      },
+    });
+
     render(
           <I18nextProvider i18n={i18n}>
               <Provider store={store}> 
@@ -70,6 +69,235 @@ describe("Forgot Component", () => {
               </Provider>
           </I18nextProvider>
       );
+
+      let RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      const leftArraowHandleDropDownbtn = screen.getByTestId("leftArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(leftArraowHandleDropDownbtn)
+      RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      let notifyPopUpbtn = screen.getByTestId("notifyPopUpbtn") as HTMLDivElement ;
+      fireEvent.click(notifyPopUpbtn)
+  });
+
+  it('renders learn react link', () => {
+
+    store = mockStore({
+      bgvReducer: {
+        employeeData: [],
+        isLoading: true,
+        employeeDataById: {"background_verification": {
+            "id": 37,
+            "user_id": 3,
+            "identity_check_documents": null,
+            "identity_check_documents_status": "in_progress",
+            "markshseet_10th": null,
+            "markshseet_10th_status": "in_progress",
+            "markshseet_12th": null,
+            "markshseet_12th_status": "in_progress",
+            "graduation_degrees": null,
+            "graduation_degrees_status": "in_progress",
+            "post_graduation_degrees": null,
+            "post_graduation_degrees_status": "in_progress",
+            "other_certifications": null,
+            "other_certifications_status": "in_progress",
+            "address_check_documents": null,
+            "address_check_documents_status": "in_progress",
+            "relieving_letters": null,
+            "relieving_letters_status": "in_progress",
+            "experience_letters": null,
+            "experience_letters_status": "in_progress",
+            "bank_statements": null,
+            "bank_statements_status": "in_progress",
+            "hold": true
+        }},
+        profileCompletionById: {},
+        sidePopUpDocNavTab: 2
+      },
+    });
+
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <DocumentDropDown />
+              </Provider>
+          </I18nextProvider>
+      );
+
+      // data-testid="notifyPopUpbtn"
+      let RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      const leftArraowHandleDropDownbtn = screen.getByTestId("leftArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(leftArraowHandleDropDownbtn)
+      RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      let notifyPopUpbtn = screen.getByTestId("notifyPopUpbtn") as HTMLDivElement ;
+      fireEvent.click(notifyPopUpbtn)
+  });
+
+  it('renders learn react link', () => {
+
+    store = mockStore({
+      bgvReducer: {
+        employeeData: [],
+        isLoading: true,
+        employeeDataById: {"background_verification": {
+            "id": 37,
+            "user_id": 3,
+            "identity_check_documents": null,
+            "identity_check_documents_status": "in_progress",
+            "markshseet_10th": null,
+            "markshseet_10th_status": "in_progress",
+            "markshseet_12th": null,
+            "markshseet_12th_status": "in_progress",
+            "graduation_degrees": null,
+            "graduation_degrees_status": "in_progress",
+            "post_graduation_degrees": null,
+            "post_graduation_degrees_status": "in_progress",
+            "other_certifications": null,
+            "other_certifications_status": "in_progress",
+            "address_check_documents": null,
+            "address_check_documents_status": "in_progress",
+            "relieving_letters": null,
+            "relieving_letters_status": "in_progress",
+            "experience_letters": null,
+            "experience_letters_status": "in_progress",
+            "bank_statements": null,
+            "bank_statements_status": "in_progress",
+            "hold": true
+        }},
+        profileCompletionById: {},
+        sidePopUpDocNavTab: 3
+      },
+    });
+
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <DocumentDropDown />
+              </Provider>
+          </I18nextProvider>
+      );
+
+      // data-testid="notifyPopUpbtn"
+      let RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      const leftArraowHandleDropDownbtn = screen.getByTestId("leftArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(leftArraowHandleDropDownbtn)
+      RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      let notifyPopUpbtn = screen.getByTestId("notifyPopUpbtn") as HTMLDivElement ;
+      fireEvent.click(notifyPopUpbtn)
+  });
+
+  it('renders learn react link', () => {
+
+    store = mockStore({
+      bgvReducer: {
+        employeeData: [],
+        isLoading: true,
+        employeeDataById: {"background_verification": {
+            "id": 37,
+            "user_id": 3,
+            "identity_check_documents": null,
+            "identity_check_documents_status": "in_progress",
+            "markshseet_10th": null,
+            "markshseet_10th_status": "in_progress",
+            "markshseet_12th": null,
+            "markshseet_12th_status": "in_progress",
+            "graduation_degrees": null,
+            "graduation_degrees_status": "in_progress",
+            "post_graduation_degrees": null,
+            "post_graduation_degrees_status": "in_progress",
+            "other_certifications": null,
+            "other_certifications_status": "in_progress",
+            "address_check_documents": null,
+            "address_check_documents_status": "in_progress",
+            "relieving_letters": null,
+            "relieving_letters_status": "in_progress",
+            "experience_letters": null,
+            "experience_letters_status": "in_progress",
+            "bank_statements": null,
+            "bank_statements_status": "in_progress",
+            "hold": true
+        }},
+        profileCompletionById: {},
+        sidePopUpDocNavTab: 4
+      },
+    });
+
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <DocumentDropDown />
+              </Provider>
+          </I18nextProvider>
+      );
+
+      // data-testid="notifyPopUpbtn"
+      let RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      const leftArraowHandleDropDownbtn = screen.getByTestId("leftArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(leftArraowHandleDropDownbtn)
+      RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      let notifyPopUpbtn = screen.getByTestId("notifyPopUpbtn") as HTMLDivElement ;
+      fireEvent.click(notifyPopUpbtn)
+  });
+
+  it('renders learn react link', () => {
+
+    store = mockStore({
+      bgvReducer: {
+        employeeData: [],
+        isLoading: true,
+        employeeDataById: {"background_verification": {
+            "id": 37,
+            "user_id": 3,
+            "identity_check_documents": null,
+            "identity_check_documents_status": "in_progress",
+            "markshseet_10th": null,
+            "markshseet_10th_status": "in_progress",
+            "markshseet_12th": null,
+            "markshseet_12th_status": "in_progress",
+            "graduation_degrees": null,
+            "graduation_degrees_status": "in_progress",
+            "post_graduation_degrees": null,
+            "post_graduation_degrees_status": "in_progress",
+            "other_certifications": null,
+            "other_certifications_status": "in_progress",
+            "address_check_documents": null,
+            "address_check_documents_status": "in_progress",
+            "relieving_letters": null,
+            "relieving_letters_status": "in_progress",
+            "experience_letters": null,
+            "experience_letters_status": "in_progress",
+            "bank_statements": null,
+            "bank_statements_status": "in_progress",
+            "hold": true
+        }},
+        profileCompletionById: {},
+        sidePopUpDocNavTab: 5
+      },
+    });
+
+    render(
+          <I18nextProvider i18n={i18n}>
+              <Provider store={store}> 
+                  <DocumentDropDown />
+              </Provider>
+          </I18nextProvider>
+      );
+
+      // data-testid="notifyPopUpbtn"
+      let RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      const leftArraowHandleDropDownbtn = screen.getByTestId("leftArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(leftArraowHandleDropDownbtn)
+      RightArraowHandleDropDownbtn = screen.getByTestId("RightArraowHandleDropDown") as HTMLDivElement ;
+      fireEvent.click(RightArraowHandleDropDownbtn)
+      let notifyPopUpbtn = screen.getByTestId("notifyPopUpbtn") as HTMLDivElement ;
+      fireEvent.click(notifyPopUpbtn)
   });
 
 })
