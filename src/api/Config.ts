ec30c9f770371cf65d8c48282bdc8b565b117044
Kallesh B S <kallesh.bs@deeporion.com>
2024-08-27 16:05:14 +0530

    domain

--- src/api/Config.ts
+++ src/api/Config.ts
@@ -1,7 +1,7 @@
 export const getHost = () => {
   const host = window.location.host;
 
-  if (host.includes("localhost") || host.includes("staging.deeporion.com") || host.includes('bgv-one.vercel.app')) {
+  if (host.includes("localhost") || host.includes("staging.deeporion.com") || host.includes('bgv-kallesh-b-ss-projects.vercel.app')) {
     // return `http://localhost:3000/`;
     // return `https://deeporionback-0cd6e44c3c14.herokuapp.com/`;
     return "https://api.staging.deeporion.com/";
